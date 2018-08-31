4219d4c6ce2ea890f76e9e6dbb82edc03d8b314d
eghan <eghant@gmail.com>
2018-08-31 16:48:04 -0700

    start tying in rootQuery, not finished/functional

--- schema/schema.js
+++ schema/schema.js
@@ -3,7 +3,26 @@ const axios = require('axios');
 const {
   GraphQLObjectType,
   GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLNonNull
 } = graphql;
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    tag: {
+      type: TagType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args){
+        return axios.get(`http://localhost:3000/companies/${args.id}`)
+        .then(resp => resp.data);
+      }
+    }
+  }
+});
+
 
 
 
